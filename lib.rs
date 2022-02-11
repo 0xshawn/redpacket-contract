3926aea6af48f300a6358ac1fe347739eccf3995
Shawn Tian <hightian@gmail.com>
2022-02-12 01:23:28 +0800

    add get_balance fn

--- lib.rs
+++ lib.rs
@@ -5,14 +5,8 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod redpacket {
-    // use ink_storage::collections::{
-    //     HashMap as StorageHashMap,
-    // };
-
     #[ink(storage)]
-    pub struct RedPacket {
-        // value: u32,
-    }
+    pub struct RedPacket {}
 
     impl RedPacket {
         #[ink(constructor, payable)]
@@ -21,9 +15,10 @@ mod redpacket {
         }
 
         #[ink(message)]
-        pub fn get_red_packet(&mut self, value: Balance) {
-            let value = 1;
+        pub fn get_red_packet(&mut self) {
+            let value = 9;
             assert!(value <= self.env().balance(), "insufficient funds!");
+            assert!(value <= 1, "insufficient funds!");
 
             if self.env().transfer(self.env().caller(), value).is_err() {
                 panic!(
@@ -33,23 +28,10 @@ mod redpacket {
                 )
             }
         }
-    }
 
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
-        use ink_lang as ink;
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut redpacket = RedPacket::new(false);
-            assert_eq!(redpacket.get(), false);
-            redpacket.flip();
-            assert_eq!(redpacket.get(), true);
+        #[ink(message)]
+        pub fn get_balance(&mut self) -> Balance {
+            return self.env().balance();
         }
     }
 }
